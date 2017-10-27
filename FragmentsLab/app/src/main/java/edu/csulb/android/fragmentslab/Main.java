7284a8daad3ee769ce1872ad0284797d4781b9a3
John Nguyen <johnkmnguyen@Johns-MBP.T-mobile.com>
2017-10-26 23:26:26 -0700

    Fragment Lab

---
+++ FragmentsLab/app/src/main/java/edu/csulb/android/fragmentslab/Main.java
@@ -0,0 +1,13 @@
+package edu.csulb.android.fragmentslab;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Main extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
